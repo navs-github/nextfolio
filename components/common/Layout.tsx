1ad5358e497283d89bd4383b604aed32eb345da2
Abhinav <abhinavverma358@gmail.com>
2022-06-19 02:36:58 -0400

    Accent set up

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 function Layout({ children }: Props) {
     return (
-        <div className="text-lg xl:text-[28px] 2xl:text-3xl text-warm selection:bg-yellow-base selection:text-black bg-dark font-inter font-light overflow-hidden">
+        <div className="text-lg xl:text-[28px] 2xl:text-3xl text-warm selection:bg-theme-accent selection:text-black bg-dark font-inter font-light overflow-hidden">
             <div className="px-6 xl:px-44 2xl:px-56 bg-grain bg-contain">
                 {children}
             </div>
