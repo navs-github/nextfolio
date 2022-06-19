b2485f1701645551b908e8b69591e9350cc007cb
Abhinav <abhinavverma358@gmail.com>
2022-06-19 04:06:36 -0400

    Introduce site-wide accent color switch

--- components/common/ArrowLinkInternal.tsx
+++ components/common/ArrowLinkInternal.tsx
@@ -12,7 +12,7 @@ function ArrowLinkInternal({ to, text }: Props) {
                 <span className="text-base xl:text-xl 2xl:text-2xl tracking-tight font-work font-medium uppercase group-hover:text-theme-accent transition-colors duration-200">
                     {text}
                 </span>
-                <svg className="h-8 w-8 text-theme-accent group-hover:text-yellow-accent group-hover:ml-4 transition-all duration-200" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 25 25">
+                <svg className="h-8 w-8 text-theme-accent group-hover:text-theme-accent group-hover:ml-4 transition-all duration-200" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 25 25">
                     <path d="m17.5 5.999-.707.707 5.293 5.293H1v1h21.086l-5.294 5.295.707.707L24 12.499l-6.5-6.5z" data-name="Right" />
                 </svg>
             </span>
--- components/common/TopNav.tsx
+++ components/common/TopNav.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Link from 'next/link'
 
 import Logo from '@components/logos/Logo'
@@ -6,13 +8,29 @@ type Props = {
     className?: string;
 }
 function TopNav({ className }: Props) {
+    const [accentColor, setAccentColor] = useState<string>('yellow');
+
     return (
         <header className={`${className} pt-8 xl:pt-12`}>
-            <div className="flex flex-col space-y-6 xl:flex-row w-full xl:space-y-0 xl:justify-between items-center">
+            <div className="w-full flex flex-col space-y-6 xl:flex-row xl:space-y-0 items-center">
                 <div className="animate-slideup">
                     <Logo />
                 </div>
-                <nav className="w-full xl:w-auto font-work font-medium text-sm xl:text-lg 2xl:text-xl">
+                <div className="ml-12 hidden xl:flex flex-row items-center space-x-2">
+                    <button
+                        className={`p-0.5 border-2 border-warm rounded-full ${accentColor == "yellow" ? "border-opacity-100" : "border-opacity-50"} hover:border-opacity-100 transition-opacity duration-200 ease overflow-hidden`}
+                        onClick={() => { setThemeColor("yellow") }}
+                    >
+                        <div className="w-4 h-4 rounded-full bg-yellow-accent"></div>
+                    </button>
+                    <button
+                        className={`p-0.5 border-2 border-warm rounded-full ${accentColor == "blue" ? "border-opacity-100" : "border-opacity-50"} hover:border-opacity-100 transition-opacity duration-200 ease group overflow-hidden`}
+                        onClick={() => { setThemeColor("blue") }}
+                    >
+                        <div className="w-4 h-4 rounded-full bg-blue-accent"></div>
+                    </button>
+                </div>
+                <nav className="ml-auto w-full xl:w-auto font-work font-medium text-sm xl:text-lg 2xl:text-xl">
                     <ul className="w-full flex flex-row justify-between xl:justify-start xl:space-x-16">
                         <li className="opacity-0 animate-slideup50 text-link">
                             <Link href="/work" passHref>
@@ -39,6 +57,17 @@ function TopNav({ className }: Props) {
             </div>
         </header>
     )
+
+    function setThemeColor(color) {
+        setAccentColor(color);
+
+        if (color == "yellow") {
+            document.body.className = "";
+        }
+        else {
+            document.body.className = `theme-${color}`;
+        }
+    }
 }
 
 export default TopNav
