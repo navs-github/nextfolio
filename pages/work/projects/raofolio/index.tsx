0a47e9f71aaf7af71f7891a9cc76c0c2d1cbe503
Abhinav <abhinavverma358@gmail.com>
2022-06-24 01:14:34 -0400

    Fix Copy

--- pages/work/projects/raofolio/index.tsx
+++ pages/work/projects/raofolio/index.tsx
@@ -27,7 +27,7 @@ function Raofolio() {
             <main ref={firstRef} className="mt-20 xl:mt-32 flex flex-col items-start">
                 <BoxHeading inView={inViewFirst}>BACKGROUND</BoxHeading>
                 <div className={"flex flex-col items-start opacity-0 " + (inViewFirst ? "animate-fadein" : "animate-none")}>
-                    <p className="mt-8 xl:mt-12 leading-[180%]">A fashion model"s online portfolio. Designed with a minimal dark theme to keep the focus on the finer details of the imagery.</p>
+                    <p className="mt-8 xl:mt-12 leading-[180%]">A fashion model's online portfolio. Designed with a minimal dark theme to keep the focus on the finer details of the imagery.</p>
                     <div className="mt-12 xl:mt-16 self-center flex flex-col">
                         <div className="relative h-96 2xl:h-[500px] p-2 border border-warm">
                             <Image src={raofolio_preview_large.src} layout="fill" className="object-cover" alt="" />
@@ -39,7 +39,7 @@ function Raofolio() {
             <section className="mt-20 xl:mt-32 pb-24 flex flex-col items-start" ref={secondRef}>
                 <BoxHeading inView={inViewSecond}>STATUS</BoxHeading>
                 <div className={"flex flex-col items-start opacity-0 " + (inViewSecond ? "animate-fadein" : "animate-none")}>
-                    <p className="mt-8 xl:mt-12 leading-[180%]">Using my own design intuition, and with my client"s artistic preferences as guidelines, I ideated, designed, and developed this portfolio in 2020.</p>
+                    <p className="mt-8 xl:mt-12 leading-[180%]">Using my own design intuition, and with my client's artistic preferences as guidelines, I ideated, designed, and developed this portfolio in 2020.</p>
                     <div className="mt-8 xl:mt-12">
                         <ArrowLinkExternal text="VISIT WEBSITE" link="https://raofolio.in/"></ArrowLinkExternal>
                     </div>
